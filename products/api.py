f0dc1c714ff4a3b0496cc8e647514a5241e8e93d
Wilson Giovanny Velandia Barreto <willyv78@gmail.com>
2016-08-17 21:34:55 -0500

    Create static.yaml
    Create static.yaml

--- products/api.py
+++ products/api.py
@@ -6,7 +6,7 @@ from .models import Product
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'name', 'price', 'quantity', 'url')
+        fields = ('id', 'name', 'price', 'quantity', 'created', 'updated', 'url')
 
 
 # Viewsets
