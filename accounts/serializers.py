c9f19417990ce6e30eecd4a0b7f6ac7d7e28e6b6
Julio Brito <jucebridu@gmail.com>
2016-09-14 16:41:54 -0500

    installed paypal, reportlab and google storage

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,6 +3,7 @@ from .models import Owner, Employee, Customer, Supplier
 
 
 class OwnerSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
 
     class Meta:
         model = Owner
@@ -10,6 +11,7 @@ class OwnerSerializer(serializers.ModelSerializer):
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
 
     class Meta:
         model = Employee
@@ -17,12 +19,15 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
 
     class Meta:
         model = Customer
         fields = ('id', 'user', 'business', 'created', 'updated', 'url')
 
+
 class SupplierSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
 
     class Meta:
         model = Supplier
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 
 from .serializers import OwnerSerializer, EmployeeSerializer, CustomerSerializer, SupplierSerializer
 from.models import Owner, Employee, Customer, Supplier
+from business.models import Business
 
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
@@ -13,42 +14,12 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_owner and user.owner.business:
-            business_query = user.owner.business.all()
-        elif user.is_employee:
-            business_query = Business.objects.filter(employees__contains=user.employee)
-        else:
-            business_query = list()
-        return self.queryset.filter(business__in=business_query)
-
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_owner and user.owner.business:
-            business_query = user.owner.business.all()
-        elif user.is_employee:
-            business_query = Business.objects.filter(employees__contains=user.employee)
-        else:
-            business_query = list()
-        return self.queryset.filter(business__in=business_query)
-
 
 class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_owner and user.owner.business:
-            business_query = user.owner.business.all()
-        elif user.is_employee:
-            business_query = Business.objects.filter(employees__contains=user.employee)
-        else:
-            business_query = list()
-        return self.queryset.filter(business__in=business_query)
