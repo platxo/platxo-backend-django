27a32760ad81f4c315904fc9ad3a1bcb9ea38cab
Julio Brito <jucebridu@gmail.com>
2016-10-09 19:21:29 -0500

    implemented send email

--- users/utils.py
+++ users/utils.py
@@ -1,2 +1,10 @@
+from django.core.mail import send_mail as _send_mail
+
 def send_mail(key, user_email):
-    pass
+    return _send_mail(
+        'Test Platxo',
+        key,
+        'admin@platxo.com',
+        [user_email],
+        fail_silently=False,
+    )
